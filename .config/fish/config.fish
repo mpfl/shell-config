55843d787d0a4615907fa3f72067321751f39042
Matthias Liffers <m@tthi.as>
2024-06-02 06:45:02 +0800

    Save config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
 end
+
+# Created by `pipx` on 2024-02-02 07:13:16
+set PATH $PATH /home/mpfl/.local/bin
